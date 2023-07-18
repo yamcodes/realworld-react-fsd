ee21209157e84f7b84cdac3f6d2bdda1b41d0def
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-07-18 17:25:35 +0300

    build: add @effector/redux-devtools-adapter

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,12 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['effector/babel-plugin'],
+        configFile: true,
+      },
+    }),
     eslint(),
     checker({ typescript: true }),
     unfonts({
