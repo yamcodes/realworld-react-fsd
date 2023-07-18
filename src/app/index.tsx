2b3e018a5150ea5c01566df22a7f02f78583e980
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-07-18 17:34:01 +0300

    feat(app): add ScopeProvider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { attachReduxDevTools } from '@effector/redux-devtools-adapter';
+import { allSettled, fork, scopeBind } from 'effector';
+import { Provider as ScopeProvider } from 'effector-react';
 import ReactDOM from 'react-dom/client';
 import { effectorSessionModel } from '~entities/session';
+import { initialize } from './initialization';
 import { Provider } from './providers';
 
 effectorSessionModel.appInitialized();
 
+export const scope = fork();
+
+await allSettled(initialize, { scope });
+
+attachReduxDevTools({
+  scope,
+  name: 'Effector devtools',
+});
+
+scopeBind(initialize, { scope })();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider />,
+  <ScopeProvider value={scope}>
+    <Provider />
+  </ScopeProvider>,
 );
