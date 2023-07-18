c5e2a0eeb2d0c9d20fde67f4ed9f3a1d3f3cb1d3
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-07-18 17:32:36 +0300

    refactor(app/providers): delete ReactQueryDevtools

--- src/app/providers/QueryClientProvider.tsx
+++ src/app/providers/QueryClientProvider.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
 import { QueryClientProvider as TanStackQueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '~shared/lib/react-query';
 
 type QueryClientProviderProps = {
@@ -12,32 +11,6 @@ export function QueryClientProvider(props: QueryClientProviderProps) {
   return (
     <TanStackQueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools
-        initialIsOpen={false}
-        errorTypes={[
-          {
-            name: '422',
-            initializer: () => ({
-              status: 422,
-              error: {
-                errors: {
-                  field1: ['error message 1', 'error message 2'],
-                  field2: ['error message 3'],
-                },
-              },
-            }),
-          },
-          {
-            name: '500',
-            initializer: () => ({
-              status: 500,
-              error: {
-                message: 'Internal server error',
-              },
-            }),
-          },
-        ]}
-      />
     </TanStackQueryClientProvider>
   );
 }
